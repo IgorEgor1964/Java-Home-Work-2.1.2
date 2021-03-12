fbf44ba020efc7e60943de1fd1b22a0a12303a4a
Igor Efimenko <igor1964egor@yandex.ru>
2021-03-12 14:51:21 +1000

    Jva Code

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
         boolean account = true;
         int limit = 1000; // если клиент пополняет счёт более чем на 1000 рублей
         int balance = 100; // на счёте 100 рублей
-        int topUp = 1100; // пополнить счет на сумму ...
+        int topUp = 2000; // пополнить счет на сумму ...
         int moneyBonus = balance + topUp + topUp/100;
         int moneyAmount = balance + topUp;
 
